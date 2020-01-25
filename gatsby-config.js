68b4cf6ad4f28844b8fa5ca6aa8202ded97d9556
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2020-01-25 16:28:31 +0300

    Set up images from MD

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,23 @@ module.exports = {
         path: `${__dirname}/src/content/blog`,
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 920,
+              quality: 90,
+            },
+          },
+        ],
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
